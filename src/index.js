3323d1cbba603ab306930936fed6f598c38cac83
pankaj <0xpankaj@gmail.com>
2023-12-31 01:45:36 +0545

    utils files is added for proper Error handling, Response handling, Response handling

--- src/index.js
+++ src/index.js
@@ -1,28 +1,22 @@
+import "dotenv/config";
 
-import 'dotenv/config';
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-import connectDB from './db/index.js';
-
-
-
-connectDB();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+connectDB()
+  .then(() => {
+    app.on("error: ", (error) => {
+      console.log("error: ", error);
+      process.exit(1);
+    });
+    app.listen(process.env.PORT || 7000, () => {
+      console.log(`App is listening on Port : `, process.env.PORT || 7000);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGO_DB connection failed : ", error);
+    process.exit(1);
+  });
 
 /* console.log(process.env);
 import express from 'express';
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+
+
+const asyncHandler = (requestHandler) => {
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((error)=> next(error))
+    }
+}
+
+
+
+/*
+
+const asyncHandler = (fn) => async(req,res,next) => {
+    try {
+        await fn(req,res,next);
+        
+    } catch (error) {
+    res.status(error.code || 500).json({
+        success: false,
+        message: error.message
+    })        
+    }
+}
+
+*/
+
+export { asyncHandler}
